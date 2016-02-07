0e8f51693b033a2c1e9b0019a93fde5929f1f792
Paul Fredrickson <qmoto@fedora-linux.shared>
2016-02-06 23:50:30 -0800

    Build on Fedora, and get inverse-alpha filter working
    It was surprisingly difficult to get the buffer and NDRange
    size and numbers right.

--- example/GEGL_invertgamma.cl
+++ example/GEGL_invertgamma.cl
@@ -1,9 +1,9 @@
 
-__kernel void hello(__global float* out,
-					__global const float* in)
+__kernel void hello(__global float4* out,
+					__global const float4* in)
 {
-	int id = get_global_id(0) * 4;
+	int id = get_global_id(0);
 
 	out[id].xyz = 1.0 - in[id].xyz;
-	out[id].a = in[id].a;
+	out[id].w = in[id].w;
 }
--- example/util_opencl.cpp
+++ example/util_opencl.cpp
@@ -17,19 +17,34 @@ using std::cerr;
 
 const bool debug = false;
 
-const char* src = "__kernel void hello(__global float4* out, \
-					__global const float4* in) \
+const char* inverse_alpha = "\
+__kernel void myfilter(	__global float4* out, \
+						__global const float4* in, \
+						__global int* ids) \
 { \
-	int id = get_global_id(0) * 4; \
+	int id = get_global_id(0); \
+	if (id > ids[0]) ids[0] = id; \
  \
 	out[id].xyz = 1.0 - in[id].xyz; \
 	out[id].w = in[id].w; \
 }";
 
+string safe(char s) {
+	switch(s) {
+		case '\t': return "\\t";
+		case '\r': return "\\r";
+		case '\n': return "\\n";
+		default: break;
+	}
+	string r(1, s);
+	return r;
+}
+
 Program::Sources getSource(const string& fname) {
-	/*std::ifstream infile(fname.c_str());
+	/*
+	std::ifstream infile(fname.c_str());
 	if (!infile) {
-		throw std::exception();
+		throw Error(1, "Source file not found");
 	}
 	std::stringstream buffer;
 	buffer << infile.rdbuf();
@@ -39,7 +54,7 @@ Program::Sources getSource(const string& fname) {
 	cerr << "Loaded program (" << src.length() << ")\n";
 	return Program::Sources(1, std::make_pair(src.c_str(), src.length()));
 	*/
-	return Program::Sources(1, std::make_pair(src, strlen(src)));
+	return Program::Sources(1, std::make_pair(inverse_alpha, strlen(inverse_alpha)));
 }
 
 void RunProgram(GEGLclass* ggObj, float* in, float* out) {
@@ -88,31 +103,39 @@ void RunProgram(GEGLclass* ggObj, float* in, float* out) {
 			}
 		}
 
-		Kernel kernel(prg, "hello", &err);
+		Kernel kernel(prg, "myfilter", &err);
 
 		CommandQueue queue(context, devices[0], CL_QUEUE_PROFILING_ENABLE, &err);
 		Event event;
 
-		::size_t bufsize = get_pixelcount(ggObj) * sizeof(float);
+		::size_t bufsize = get_pixelcount(ggObj) * sizeof(float) * 4;
 		Buffer inbuf(context, CL_MEM_READ_ONLY, bufsize);
 		Buffer outbuf(context, CL_MEM_WRITE_ONLY, bufsize);
+		Buffer scratch(context, CL_MEM_READ_WRITE, 32);
 
-		queue.enqueueWriteBuffer(inbuf, CL_FALSE, 0, bufsize, in);
+		int* scratch_buf = (int*)malloc(32);
+		memset(scratch_buf, 0, 8);
+
+		queue.enqueueWriteBuffer(inbuf, CL_TRUE, 0, bufsize, in);
+		queue.enqueueWriteBuffer(scratch, CL_TRUE, 0, 32, scratch_buf);
 
 		kernel.setArg(0, outbuf);
 		kernel.setArg(1, inbuf);
+		kernel.setArg(2, scratch);
 
 		queue.enqueueNDRangeKernel(
 		  kernel, 
 		  NullRange, 
-		  NDRange(bufsize),
+		  NDRange(get_pixelcount(ggObj)),
 		  NullRange,
 		  NULL,
-		  &event); 
+		  &event);
 
 		event.wait();
 
+		queue.enqueueReadBuffer(scratch, CL_TRUE, 0, 8, scratch_buf);
 		queue.enqueueReadBuffer(outbuf, CL_TRUE, 0, bufsize, out);
+		cerr << "max work item " << scratch_buf[0] << "\n";
 	}
 	#ifdef __CL_ENABLE_EXCEPTIONS
 	catch (cl::Error err) {
